44ffece8c2c9c3a54164140338a25c50a6dfc699
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-05-12 16:07:41 +0200

    [feat] basic timeline
    timeline ontvangt startdate en enddate
    enkel small lines verspreiden per jaar
    
    ziet er nog niet uit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
+	<!--
   <h1>De Koude oorlog</h1>
   <h2>De berlijnse muur</h2>
   <p>De val van de berlijnse muur</p>
   <line-big :year="1989" />
   <line-current :year="1989" />
   <line-small />
+-->
+	<time-line :startyear="1945" :endyear="1989"/>
 </template>
 
 <script setup>
 import lineBig from "@/components/timeline/lines/line-big.vue";
 import lineSmall from "@/components/timeline/lines/line-small.vue";
 import lineCurrent from "@/components/timeline/lines/line-current.vue";
+import timeLine from "@/components/timeline/time-line.vue";
 </script>
 
 <style lang="scss">
---
+++ src/components/timeline/time-line.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="wrapper">
+    <div id="base"></div>
+		<div id="line-wrapper">
+    <div id="lines" v-for="year in years" :key="year">
+			<line-small />
+		</div>
+		</div>
+  </div>
+</template>
+
+<script setup>
+import lineBig from "@/components/timeline/lines/line-big.vue";
+import lineSmall from "@/components/timeline/lines/line-small.vue";
+import lineCurrent from "@/components/timeline/lines/line-current.vue";
+import {defineProps} from 'vue';
+
+const props = defineProps({
+  startyear: {
+    type: Number,
+    required: true,
+  },
+  endyear: {
+    type: Number,
+    required: true,
+  },
+});
+
+const years = Array.from({ length: props.endyear - props.startyear + 1 }, (_, i) => props.startyear + i);
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
+
+#wrapper {
+  width: 95%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#base {
+  background-color: $usa-blue;
+  color: $usa-blue;
+  width: 100%;
+  height: 19px;
+  border-radius: 16px;
+}
+#line-wrapper {
+	width: 100%;
+	margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
