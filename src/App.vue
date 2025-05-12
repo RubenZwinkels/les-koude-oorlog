160d95cbf11ed5f6abaecac019e86e9b62ade557
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-05-12 12:22:20 +0200

    [refac] variables verplaatst naar variables.scss voor import functies

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,11 @@
 	<h1>De Koude oorlog</h1>
 	<h2>De berlijnse muur</h2>
 	<p>De val van de berlijnse muur</p>
+	<line-small />
 </template>
 
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');
-// colors
-$primary-color: #730D12;
-$secondary-color: #0A305C; 
-$bg-color: #ABABA1;
-$black-color: #171B26;
+<script setup>
+import lineSmall from "@/components/timeline/lines/line-small.vue"
+</script>
 
-// fonts
-h1, h2 {
-  font-family: "Russo One", sans-serif;
-  color: $black-color;
-}
-
-h1 {
-  font-size: 42px;
-}
-
-h2 {
-  font-size: 26px;
-}
-
-p {
-  font-family: "Inter", sans-serif;
-  font-size: 15px;
-}
-</style>
+<style lang="scss"></style>
