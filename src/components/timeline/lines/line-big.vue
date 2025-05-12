34971152b3718cc9b670d6ceb1ae9014e8245b54
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-05-12 13:39:38 +0200

    [feat] line big toegevoegd

---
+++ src/components/timeline/lines/line-big.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="line"></div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
+
+.line {
+  color: $sov-red;
+  background-color: $sov-red;
+  width: 4px;
+  height: 35px;
+  border-radius: 5px;
+  border-width: 1px;
+}
+</style>
