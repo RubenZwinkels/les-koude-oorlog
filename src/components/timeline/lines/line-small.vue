fe571a76a19f8c18f0be9d49b485483dd78320ed
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-05-12 13:29:05 +0200

    [feat] line-small component toegevoegd
    component voor de timeline

---
+++ src/components/timeline/lines/line-small.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="line-small"></div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
+
+.line-small {
+  color: $usa-blue;
+  background-color: $usa-blue;
+  width: 2px;
+  height: 25px;
+  position: absolute;
+  top: 353px;
+  left: 107px;
+  border-radius: 5px;
+  border-width: 1px;
+}
+</style>
