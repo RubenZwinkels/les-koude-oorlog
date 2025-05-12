77c848ce0c6da6c44b3ddab0102fae1abc7200f7
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-05-12 14:55:29 +0200

    [feat] line current toegevoegd
    ontvangt jaartal

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-	<h1>De Koude oorlog</h1>
-	<h2>De berlijnse muur</h2>
-	<p>De val van de berlijnse muur</p>
-	<line-big :year="1989"/>
-	<line-small />
+  <h1>De Koude oorlog</h1>
+  <h2>De berlijnse muur</h2>
+  <p>De val van de berlijnse muur</p>
+  <line-big :year="1989" />
+  <line-current :year="1989" />
+  <line-small />
 </template>
 
 <script setup>
-import lineBig from "@/components/timeline/lines/line-big.vue"
-import lineSmall from  "@/components/timeline/lines/line-small.vue"
+import lineBig from "@/components/timeline/lines/line-big.vue";
+import lineSmall from "@/components/timeline/lines/line-small.vue";
+import lineCurrent from "@/components/timeline/lines/line-current.vue";
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "@/styles/variables.scss";
+</style>
---
+++ src/components/timeline/lines/line-current.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="wrapper">
+    <div class="line"></div>
+    <h2>{{ year }}</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  year: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
+
+.line {
+  color: $sov-red;
+  background-color: $sov-red;
+  width: 7px;
+  height: 44px;
+  border-radius: 5px;
+  border-width: 1px;
+	margin: 0px;
+}
+
+#wrapper {
+	width: 60px;
+	height: 150px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+h2 {
+	background-color: $sov-red;
+	color: white;
+	height: 30px;
+	width: 70px;
+	margin: -2px;
+}
+</style>
